d6c69aa440237e345ff988b0869bde91c0518401
deliwalayash <deliwalayash2@gmail.com>
2025-11-20 18:00:30 +0530

    table added

--- Formvalidation/src/App.jsx
+++ Formvalidation/src/App.jsx
@@ -15,6 +15,7 @@ function App() {
     course:"",
   })
   const [err,setErr]=useState({})
+  const [users,setUsers]=useState([])
 
   const handleChange= (e)=>{
   
@@ -50,8 +51,15 @@ function App() {
     if(Object.keys(newError).length > 0){
       return
     }
-
-    formref.current.submit()
+    setUsers([...users,user])
+    // formref.current.submit()
+    setUser({
+       name:"",
+    email:"",
+    password:"",
+    cpassword:"",
+    course:"",
+    })
 
   }
 
@@ -105,9 +113,57 @@ function App() {
    <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
  </div>
 </form>
+
+<div class="relative overflow-x-auto container mx-auto my-8">
+    <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+                <th scope="col" class="px-6 py-3">
+                    User name
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    User Email
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    User Course
+                </th>
+              
+            </tr>
+        </thead>
+        <tbody>
+          {
+            users.map((curEle)=>{
+              return(
+        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
+              <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    {curEle.name}
+                </th>
+                <td class="px-6 py-4">
+                      {curEle.email}
+                </td>
+                <td class="px-6 py-4">
+                    {curEle.course}
+                </td>
+          
+         </tr>
+              )
+            })
+          }
+            
+               
+               
+          
+           
+        </tbody>
+    </table>
+</div>
 </div>
 
+    
+   
 
+
+    
     </>
   )
 }
